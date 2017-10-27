fccaa9b1bddf240a5c28ffe1900f93605c5184e2
Waaflee <agustinwafle@gmail.com>
2017-10-26 21:04:28 -0300

    ..

--- placas/AMBIENTE/AMBIENTE.ino
+++ placas/AMBIENTE/AMBIENTE.ino
@@ -1,13 +1,7 @@
-// Prueba del módulo de sensores ambientales.
-// El sistema cuenta con sensor de temperatura externa, 
-// interna, movimiento y 2 sensores de contacto seco.
-// Programador: Moyano Jonathan.
-// Versión: 0.1
-
-// Librería para manejar las comunicaciones WIFI. 
-#include <ESP8266WiFi.h>          
+ 
+#include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>  
+#include <ESP8266HTTPClient.h>
 
 ESP8266WiFiMulti WiFiMulti;
 HTTPClient http;
@@ -50,60 +44,58 @@ Button AUX2(SENSOR_AUX2);
 void setup() {
 
  // Configuramos el puerto serial.
-  Serial.begin(115200); 
+  Serial.begin(115200);
  // Configuramos los pines utilizados.
  pinMode(SENSOR_PIR,INPUT);
  pinMode(ERROR_LED,OUTPUT);
  pinMode(OK_LED,OUTPUT);
  AUX1.begin();
  AUX2.begin();
- 
+
  // Inicializa el sensor DS18B20.
   sensors.begin();
 
   // Inicializa el módulo wifi.
    WiFi.begin("IMD-AP4", "imdwifi4");
-  
+
 }
 
 
 void loop()
-{  
+{
 
   currentMillis = millis();  // Obtiene el tiempo transcurrido en ms.
-  
+
   Serial.print("Adquiriendo temperaturas...");
   sensors.requestTemperatures(); // Enviamos el comando para adquirir la temperatura.
   Serial.println("Conversion lista...");
   Serial.print("Temperatura externa: ");
   // Obtenemos el valor de la temperatura.
-  Serial.println(sensors.getTempCByIndex(0)); 
+  Serial.println(sensors.getTempCByIndex(0));
 
    if (AUX1.pressed())
    Serial.println("VENTANA ABIERTA !!");
-  
+
   if (AUX2.released())
     Serial.println("PUERTA ABIERTA !!");
 
  // Cada 30 segundos, enviamos los datos al servidor.
   if(currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;   
+    previousMillis = currentMillis;
 
    //Inicia la transmision de datos
   http.begin("http://172.23.200.108/corriente.php?ipsrc=Oficina_1&corriente="); // HTTP.
 
   int httpCode = http.GET();
-  
+
     if(httpCode > 0) {
         if(httpCode == HTTP_CODE_OK) {
             String payload = http.getString();
         }
     } else {
     }
- 
+
     http.end();
-   
+
   }
 }
-  
-
